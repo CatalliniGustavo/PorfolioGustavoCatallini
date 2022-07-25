ce4f523a0f3a75870dcafa5fa9a52da32f523184
CatalliniGustavo <catallinigustavo@gmail.com>
2022-07-24 21:53:59 -0300

    pass y stylos

--- BackEnd/pgc/src/main/java/com/catallinigustavo/pgc/Security/Controller/AuthController.java
+++ BackEnd/pgc/src/main/java/com/catallinigustavo/pgc/Security/Controller/AuthController.java
@@ -45,10 +45,16 @@ public class AuthController {
     
     @PostMapping("/nuevo")
     public ResponseEntity<?> nuevo(@Valid @RequestBody NuevoUsuario nuevoUsuario, BindingResult bindingResult){
-        if (bindingResult.hasErrors())
-            return new ResponseEntity(new Mensaje("Campos mal puestos o email invalido"), HttpStatus.BAD_REQUEST);
+        
+        if (nuevoUsuario.getPassword().length() != 8 )
+            return new ResponseEntity(new Mensaje("La contraseña tiene que tener 8 caracteres"), HttpStatus.BAD_REQUEST);
+        if (nuevoUsuario.getApellido() == null || nuevoUsuario.getNombre() == null)
+            return new ResponseEntity(new Mensaje("Falta nombre 0/y apellido"), HttpStatus.BAD_REQUEST);
         if (usuarioService.existsByEmail(nuevoUsuario.getEmail()))
             return new ResponseEntity(new Mensaje("Ese Email ya esta relacionado a otra cuenta"), HttpStatus.BAD_REQUEST);
+        if (bindingResult.hasErrors())
+            return new ResponseEntity(new Mensaje("Campos mal puestos o email invalido"), HttpStatus.BAD_REQUEST);
+        
 //        if (usuarioService.existsByNombre(nuevoUsuario.getNombre())) ***Para ortros Valores****
 //            return new ResponseEntity(new Mensaje("Ese Nombre ya esta relacionado a otra cuenta"), HttpStatus.BAD_REQUEST);
         Usuario usuario = new Usuario (
